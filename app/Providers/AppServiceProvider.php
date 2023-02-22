8779fde7bf6b83a2a4cd92569790deacfd210179
Felix Oduor <felixochieng21@gmail.com>
2023-02-22 21:05:21 +0300

    fix public path issue

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 
+use Illuminate\Support\Facades\App;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
@@ -15,7 +16,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if(App::environment('production')){
+            $this->app->bind('path.public', function() {
+                return base_path().'/public_html';
+            });
+        }
     }
 
     /**
